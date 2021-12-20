42b6f291321505b249b56d368504085c730c8a6b
Wenisaputri <96043348+Wenisaputri@users.noreply.github.com>
2021-12-21 01:26:15 +0700

    Create dpvpn.sh

---
+++ dpvpn.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+
+apt-get install curl -y
+rm -f sc.sh
+rm -f setup.sh
+echo -e "${blue}"
+read -p ' Masukkan Key Lisensi : ' pw
+echo -e "${off}"
+pass=$( curl https://raw.githubusercontent.com/Wenisaputri/dpvpn/surah | grep $pw )
+if [ $pw = $pass ]; then
+    clear
+    sleep 0.5
+    echo -e "${green} Sukses Verifikasi Lisensi!${off}"
+    sleep 0.5
+    wget https://raw.githubusercontent.com/Wenisaputri/dpvpn/main/F.sh && chmod +x F.sh
+    ./F.sh
+else 
+    clear
+    sleep 0.5
+    echo -e "${red} Gagal Verifikasi Lisensi! ${off}"
+    sleep 2
+    rm -f F.sh
+    rm -f xoxo.sh
+    rm -f setup.sh
+    exit 0
+fi
+
+rm -f xoxo.sh
+rm -f xoxoxo.sh
+rm -f setup.sh
